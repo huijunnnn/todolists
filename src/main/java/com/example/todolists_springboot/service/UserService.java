aadf32bfc8f546e8f4d1fe79d9ca09dcd8414583
huijunnn <huijun.hong@thoughtworks.com>
2022-10-31 17:27:03 +0800

    feat: add an userService file

---
+++ src/main/java/com/example/todolists_springboot/service/UserService.java
@@ -0,0 +1,57 @@
+package com.example.todolists_springboot.service;
+
+import com.example.todolists_springboot.domain.User;
+import com.example.todolists_springboot.handler.exception.UserNotFoundException;
+import com.example.todolists_springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User addUser(User user){
+        return userRepository.save(user);
+    }
+    public List<User> getUsers(){
+        return getAllUsers();
+
+    }
+
+    private List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    private List<User> getUsers(String name) {
+        return userRepository.findByUserName(name);
+    }
+
+    private List<User> getUsersOfTaskByTaskId(Long id){
+        return userRepository.findByTaskId(id);
+    }
+    private List<User> getUsersOfTaskByTaskName(String name){
+        return userRepository.findByTaskName(name);
+    }
+
+    public User updateUser(Long id, User newUser) {
+        Optional<User> oldUser = userRepository.findById(id);
+        User user = oldUser.orElseThrow(UserNotFoundException::new);
+        newUser.setUserId(oldUser.get().getUserId());
+        return userRepository.save(newUser);
+    }
+
+    public void deleteUser(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException();
+        } else {
+            userRepository.delete(user.get());
+        }
+    }
+
+}
