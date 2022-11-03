c529d60c8e31d72798ecc4e7597105fce2de3b3d
huijunnn <huijun.hong@thoughtworks.com>
2022-11-03 16:09:27 +0800

    refactor: modify the deleteUser method.

--- src/main/java/com/example/todolists_springboot/service/UserService.java
+++ src/main/java/com/example/todolists_springboot/service/UserService.java
@@ -15,36 +15,23 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
-
     public User addUser(User user) {
         return userRepository.save(user);
     }
-
     public List<User> getUsers() {
         return getAllUsers();
-
     }
-
     private List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    private List<User> getUsers(String name) {
+    public List<User> getUsers(String name) {
         return userRepository.findByUserName(name);
     }
 
     public Optional<User> getUserById(Long id) {
         return userRepository.findById(id);
     }
-
-    public List<User> getUsersOfTaskByTaskId(Long id) {
-        return userRepository.findByTaskId(id);
-    }
-
-    public List<User> getUsersOfTaskByTaskName(String name) {
-        return userRepository.findByTaskName(name);
-    }
-
     public User updateUser(Long id, User newUser) {
         Optional<User> oldUser = userRepository.findById(id);
         User user = oldUser.orElseThrow(UserNotFoundException::new);
@@ -57,7 +44,7 @@ public class UserService {
         if (user.isEmpty()) {
             throw new UserNotFoundException();
         } else {
-            userRepository.delete(user.get());
+            userRepository.deleteById(id);
         }
     }
 
