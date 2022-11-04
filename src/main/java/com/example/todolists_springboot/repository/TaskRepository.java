b8abef4042b01f5df5c37054d39945527d53a1e5
huijunnn <huijun.hong@thoughtworks.com>
2022-11-04 17:18:22 +0800

    feat: find the last task.

--- src/main/java/com/example/todolists_springboot/repository/TaskRepository.java
+++ src/main/java/com/example/todolists_springboot/repository/TaskRepository.java
@@ -27,4 +27,7 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Query(value = "SELECT t FROM Task AS t WHERE t.taskName LIKE %:keyword%")
     List<Task> findByTaskKeyword(@Param("keyword")String keyword);
+
+    @Query(value = "SELECT t FROM Task t WHERE t.taskId = (SELECT max(t.taskId) FROM Task t)")
+    Task findLastTask();
 }
