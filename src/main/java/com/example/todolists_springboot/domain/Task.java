451006346210f9a00d3f3cae2010788947bec613
huijunnn <huijun.hong@thoughtworks.com>
2022-11-04 17:17:29 +0800

    refactor: add new annotation to solve the ContentType exception.

--- src/main/java/com/example/todolists_springboot/domain/Task.java
+++ src/main/java/com/example/todolists_springboot/domain/Task.java
@@ -1,9 +1,11 @@
 package com.example.todolists_springboot.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -25,13 +27,23 @@ public class Task implements Serializable {
     @Column(name = "task_completed")
     private Boolean taskCompleted;
 
-    @ManyToMany(mappedBy = "tasks", fetch = FetchType.LAZY)
-    private List<User> users;
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },mappedBy = "tasks")
+    @JsonIgnore
+    private List<User> users = new ArrayList<>();
 
     public Task(Long taskId, String taskName, Boolean taskCompleted) {
         this.taskId = taskId;
         this.taskName = taskName;
         this.taskCompleted = taskCompleted;
+    }
+
+    public Task(Long taskId, String taskName) {
+        this.taskId = taskId;
+        this.taskName = taskName;
     }
 
     public Task(String taskName) {
@@ -48,5 +60,9 @@ public class Task implements Serializable {
     public Task(String taskName, List<User> users) {
         this.taskName = taskName;
         this.users = users;
+    }
+
+    public List<User> getUsers() {
+        return users;
     }
 }
