a1f890d028dcaa37975fd0a1141065c2267813c1
huijunnn <huijun.hong@thoughtworks.com>
2022-11-07 16:16:48 +0800

    feat: add a handler to resolve the messsage response.

---
+++ src/main/java/com/example/todolists_springboot/handler/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.example.todolists_springboot.handler;
+
+import com.example.todolists_springboot.handler.exception.TaskNotFoundException;
+import com.example.todolists_springboot.handler.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResult handle(TaskNotFoundException exception) {
+        return new ErrorResult(exception.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResult handle(UserNotFoundException exception) {
+        return new ErrorResult(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ErrorResult handle(MethodArgumentNotValidException exception) {
+        String message = exception.getBindingResult().getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining(";"));
+        return new ErrorResult(message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorResult handle(Exception exception) {
+        return new ErrorResult(exception.getMessage());
+    }
+}
