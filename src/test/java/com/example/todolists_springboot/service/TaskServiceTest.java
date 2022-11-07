61b97c19be21e25439a218ba686c58763af152c3
huijunnn <huijun.hong@thoughtworks.com>
2022-11-07 16:15:04 +0800

    refactor: reforamt the code and modify the code.

--- src/test/java/com/example/todolists_springboot/service/TaskServiceTest.java
+++ src/test/java/com/example/todolists_springboot/service/TaskServiceTest.java
@@ -40,67 +40,74 @@ public class TaskServiceTest {
         assertEquals(result, returnedTask);
         verify(taskRepository).save(newTask);
     }
+
     @Test
-    public void should_get_all_the_tasks_when_there_is_no_parameters(){
+    public void should_get_all_the_tasks_when_there_is_no_parameters() {
         List<Task> returnedTasks = List.of(new Task(1L, "Test", false),
                 new Task(2L, "Test", true));
         when(taskRepository.findAll()).thenReturn(returnedTasks);
         List<Task> result = taskService.getTasks(null);
-        assertEquals(result,returnedTasks);
+        assertEquals(result, returnedTasks);
         verify(taskRepository).findAll();
     }
 
     @Test
-    public void should_get_all_the_tasks_when_the_parameter_is_assigned_to_true(){
+    public void should_get_all_the_tasks_when_the_parameter_is_assigned_to_true() {
         List<Task> returnedTasks = List.of(new Task(1L, "Test", true));
         when(taskRepository.findByTaskCompleted(true)).thenReturn(returnedTasks);
         List<Task> result = taskService.getTasks(true);
-        assertEquals(result,returnedTasks);
+        assertEquals(result, returnedTasks);
         verify(taskRepository).findByTaskCompleted(true);
     }
+
     @Test
-    public void should_get_all_the_tasks_when_the_parameter_is_assigned_to_false(){
+    public void should_get_all_the_tasks_when_the_parameter_is_assigned_to_false() {
         List<Task> returnedTasks = List.of(new Task(1L, "Test", false));
         when(taskRepository.findByTaskCompleted(false)).thenReturn(returnedTasks);
         List<Task> result = taskService.getTasks(false);
-        assertEquals(result,returnedTasks);
+        assertEquals(result, returnedTasks);
         verify(taskRepository).findByTaskCompleted(false);
     }
+
     @Test
-    public void should_update_the_task_by_id_when_the_task_exists(){
+    public void should_update_the_task_by_id_when_the_task_exists() {
         Task requestBody = new Task("task");
-        Task newTask = new Task(1L,"task",false);
+        Task newTask = new Task(1L, "task", false);
         Optional<Task> newTaskOptional = Optional.of(newTask);
         when(taskRepository.findById(1L)).thenReturn(newTaskOptional);
         when(taskRepository.save(newTask)).thenReturn(newTask);
-        Task result = taskService.updateTask(1L,requestBody);
-        assertEquals(result,newTask);
+        Task result = taskService.updateTask(1L, requestBody);
+        assertEquals(result, newTask);
         verify(taskRepository).findById(1L);
         verify(taskRepository).save(requestBody);
     }
+
     @Test
-    public void should_update_the_task_by_id_when_the_task_does_not_exist(){
+    public void should_update_the_task_by_id_when_the_task_does_not_exist() {
         Task requestBody = new Task("task");
         Optional<Task> newTaskOptional = Optional.empty();
         when(taskRepository.findById(1L)).thenReturn(newTaskOptional);
-        assertThrows(TaskNotFoundException.class,()->taskService.updateTask(1L,requestBody));
+        assertThrows(TaskNotFoundException.class, () -> taskService.updateTask(1L, requestBody));
         verify(taskRepository).findById(1L);
     }
+
     @Test
-    public void should_delete_the_task_when_the_task_exist(){
+    public void should_delete_the_task_when_the_task_exist() {
         Optional<Task> taskOptional = Optional.of(new Task(1L, "test", false));
         when(taskRepository.findById(1L)).thenReturn(taskOptional);
         taskService.deleteTask(1L);
         verify(taskRepository).findById(1L);
         verify(taskRepository).delete(taskOptional.get());
     }
+
     @Test
-    public void should_delete_the_task_when_the_task_dose_not_exist(){
+    public void should_delete_the_task_when_the_task_dose_not_exist() {
         Optional<Task> taskOptional = Optional.empty();
         when(taskRepository.findById(1L)).thenReturn(taskOptional);
-        assertThrows(TaskNotFoundException.class,()->taskService.deleteTask(1L));
+        assertThrows(TaskNotFoundException.class, () -> taskService.deleteTask(1L));
         verify(taskRepository).findById(1L);
     }
+
     @Test
     public void should_add_an_user_to_the_task_and_return_the_task() {
         User user = new User("zhizhi");
--- src/test/java/com/example/todolists_springboot/service/TaskUserServiceTest.java
+++ src/test/java/com/example/todolists_springboot/service/TaskUserServiceTest.java
@@ -2,6 +2,8 @@ package com.example.todolists_springboot.service;
 
 import com.example.todolists_springboot.domain.Task;
 import com.example.todolists_springboot.domain.User;
+import com.example.todolists_springboot.handler.exception.TaskNotFoundException;
+import com.example.todolists_springboot.handler.exception.UserNotFoundException;
 import com.example.todolists_springboot.repository.TaskRepository;
 import com.example.todolists_springboot.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -12,13 +14,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static java.util.stream.Stream.of;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,57 +33,7 @@ public class TaskUserServiceTest {
     @InjectMocks
     private TaskUserService taskUserService;
 
-    @Test
-    public void delete_all_tasks_of_the_user_when_tasks_exist_and_return_the_user() {
-        List<Task> savedTasks = List.of(new Task(1L, "task1", false));
-        User savedUser = new User(1L, "zhizhi", savedTasks);
-        when(userRepository.findById(1L)).thenReturn(Optional.of(savedUser));
-        User result = taskUserService.deleteAllTasksOfUserByUserId(1L).get();
-        User finalUser = new User(1L, "zhizhi", null);
-        assertEquals(result, finalUser);
-        verify(userRepository, times(2)).findById(1L);
-    }
 
-    @Test
-    public void delete_all_tasks_of_the_user_when_tasks_not_exist_and_return_the_user() {
-        User savedUser = new User(1L, "zhizhi");
-        when(userRepository.findById(1L)).thenReturn(Optional.of(savedUser));
-        User result = taskUserService.deleteAllTasksOfUserByUserId(1L).get();
-        User finalUser = new User(1L, "zhizhi", null);
-        assertEquals(result, finalUser);
-        verify(userRepository, times(2)).findById(1L);
-    }
-    @Test
-    public void add_shared_tasks_to_the_user_and_when_sharedUser_tasks_exist_return_the_tasks(){
-        List<Task> tasks1 = List.of(new Task(1L, "task1", false));
-        User userOne = new User(1L,"zhizhi",tasks1);
-        List<Task> tasks2 = List.of(new Task(2L, "task2", false));
-        User userTwo = new User(2L,"lacy",tasks2);
-        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
-        when(userRepository.findById(2L)).thenReturn(Optional.of(userTwo));
-        List<Task> tasks = of(tasks1, tasks2)
-                .flatMap(Collection::stream)
-                .distinct()
-                .collect(Collectors.toList());
-        List<Task> resultedTasks = taskUserService.addSharedTasks(2L,1L);
-        assertEquals(tasks,resultedTasks);
-        verify(userRepository,times(4)).findById(1L);
-        verify(userRepository,times(2)).findById(2L);
-
-    }
-    @Test
-    public void add_shared_tasks_to_the_user_when_shardUser_tasks_not_exist_and_return_the_tasks(){
-        List<Task> tasks1 = List.of(new Task(1L, "task1", false));
-        User userOne = new User(1L,"zhizhi",tasks1);
-        User userTwo = new User(2L,"lacy");
-        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
-        when(userRepository.findById(2L)).thenReturn(Optional.of(userTwo));
-        List<Task> resultedTasks = taskUserService.addSharedTasks(2L,1L);
-        assertEquals(tasks1,resultedTasks);
-        verify(userRepository,times(3)).findById(1L);
-        verify(userRepository,times(2)).findById(2L);
-
-    }
     @Test
     public void get_all_tasks_by_user_name_and_return_the_tasks(){
         List<Task> taskOne = List.of(new Task(1L,"task1",false)
@@ -124,7 +74,6 @@ public class TaskUserServiceTest {
         List<User> resultUsers = taskUserService.getUsersOfTaskByTaskName("task1");
         assertEquals(userOne, resultUsers);
         verify(userRepository).findByTaskName("task1");
-
     }
 
     @Test
@@ -138,6 +87,202 @@ public class TaskUserServiceTest {
         when(userRepository.findByTaskId(1L)).thenReturn(userOne);
         List<User> resultUsers = taskUserService.getUsersOfTaskByTaskId(1L);
         assertEquals(userOne, resultUsers);
+        verify(userRepository).findByTaskId(1L);
+    }
+
+    @Test
+    public void should_add_a_task_to_the_user_when_the_user_not_exist() {
+        Optional<User> userOptional = Optional.empty();
+        Task task = new Task("task1");
+        when(userRepository.findById(1L)).thenReturn(userOptional);
+        assertThrows(UserNotFoundException.class, () -> taskUserService.createTaskForUser(1L, task));
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_add_a_task_to_the_user_when_the_task_not_exist_in_the_tasks_table() {
+        Task task = new Task("task1");
+        List<Task> tasks = List.of(task);
+        User savedUser = new User(1L, "zhizhi");
+        Task savedTask = new Task(1L, "task1", false);
+        User returnedUser = new User(1L, "zhizhi", List.of(savedTask));
+        Task returnedTask = new Task(1L, "task1", false, List.of(savedUser));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(savedUser));
+        when(taskRepository.save(task)).thenReturn(savedTask);
+        when(userRepository.save(savedUser)).thenReturn(returnedUser);
+        when(taskRepository.findLastTask()).thenReturn(savedTask);
+        Task result = taskUserService.createTaskForUser(1L, task);
+        assertEquals(savedTask, result);
+        verify(userRepository).findById(1L);
+        verify(taskRepository).save(task);
+        verify(taskRepository).findLastTask();
+
+    }
+
+    @Test
+    public void should_add_a_task_to_the_user_when_the_task_exist_in_the_tasks_table_and_belong_to_the_user() {
+        Task task = new Task("task1");
+        List<Task> tasks = List.of(task);
+        User savedUser = new User(1L, "zhizhi");
+        Task savedTask = new Task(1L, "task1", false);
+        User returnedUser = new User(1L, "zhizhi", List.of(savedTask));
+        Task returnedTask = new Task(1L, "task1", false, List.of(savedUser));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(returnedUser));
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(returnedTask));
+        when(userRepository.findByTaskId(1L)).thenReturn(List.of(savedUser));
+        when(userRepository.save(savedUser)).thenReturn(returnedUser);
+        Task result = taskUserService.createTaskForUser(1L, task);
+        assertEquals(savedTask, result);
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_add_a_task_to_the_user_when_the_task_exist_in_the_tasks_table_and_not_belong_to_the_user() {
+        Task task = new Task("task1");
+        List<Task> tasks = List.of(task);
+        User savedUser = new User(1L, "zhizhi");
+        Task savedTask = new Task(1L, "task1", false);
+        Task returnedTask = new Task(1L, "task1", false, List.of(savedUser));
+        User returnedUser = new User(1L, "zhizhi", List.of(savedTask));
+        when(userRepository.findById(1L)).thenReturn(Optional.of(savedUser));
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(savedTask));
+        when(userRepository.findByTaskId(1L)).thenReturn(new ArrayList<>());
+        Task result = taskUserService.createTaskForUser(1L, task);
+        assertEquals(savedTask, result);
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_delete_all_tasks_of_the_user_when_the_user_not_exist() {
+        Optional<User> userOptional = Optional.empty();
+        Task task = new Task("task1");
+        when(userRepository.findById(1L)).thenReturn(userOptional);
+        assertThrows(UserNotFoundException.class, () -> taskUserService.deleteAllTasksOfUserByUserId(1L));
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_delete_all_tasks_of_the_user_when_the_user_exist_and_tasks_is_not_null() {
+
+        Task task = new Task("task1");
+        List<Task> tasks = List.of(task);
+        User savedUser = new User(1L, "zhizhi", tasks);
+        User user = new User(1L, "zhizhi");
+        when(userRepository.findById(1L)).thenReturn(Optional.of(savedUser));
+        Object result = taskUserService.deleteAllTasksOfUserByUserId(1L);
+        assertNull(result);
+        verify(userRepository).findById(1L);
+        verify(userRepository).save(user);
+
+    }
+
+    @Test
+    public void should_delete_all_tasks_of_the_user_when_the_user_exist_and_tasks_is_null() {
+        User savedUser = new User(1L, "zhizhi", null);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(savedUser));
+        Object result = taskUserService.deleteAllTasksOfUserByUserId(1L);
+        assertNull(result);
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_share_tasks_to_the_user_when_the_user_not_exist() {
+        Optional<User> userOptional = Optional.empty();
+        when(userRepository.findById(1L)).thenReturn(userOptional);
+        assertThrows(UserNotFoundException.class, () -> taskUserService.addSharedTasks(1L, 2L));
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_share_tasks_to_the_user_when_the_sharedUser_not_exist() {
+        Optional<User> userOptional = Optional.empty();
+        when(userRepository.findById(2L)).thenReturn(userOptional);
+        assertThrows(UserNotFoundException.class, () -> taskUserService.addSharedTasks(1L, 2L));
+        verify(userRepository).findById(2L);
+    }
+
+    @Test
+    public void should_share_tasks_to_the_user_when_the_users_exist_and_shared_tasks_is_not_null() {
+        List<Task> tasks1 = List.of(new Task(1L, "task1", false), new Task(2L, "task2", false));
+        List<Task> tasks2 = List.of(new Task(3L, "task3", false));
+        User userOne = new User(1L, "zhizhi", tasks1);
+        User userTwo = new User(2L, "zhi", tasks2);
+        List<Task> tasks3 = List.of(new Task(1L, "task1", false), new Task(2L, "task2", false),
+                new Task(3L, "task3", false));
+        User theReturnedUser = new User(2L, "zhi", tasks3);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
+        when(userRepository.findById(2L)).thenReturn(Optional.of(userTwo));
+        when(userRepository.save(userOne)).thenReturn(theReturnedUser);
+        List<Task> result = taskUserService.addSharedTasks(1L, 2L);
+        assertEquals(tasks3, result);
+        verify(userRepository, times(2)).findById(1L);
+        verify(userRepository, times(2)).findById(2L);
+    }
+
+    @Test
+    public void should_share_tasks_to_the_user_when_the_users_exist_and_shared_tasks_is_null() {
+        List<Task> tasks = List.of(new Task(3L, "task3", false));
+        User userOne = new User(1L, "zhizhi");
+        User userTwo = new User(2L, "zhi", tasks);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
+        when(userRepository.findById(2L)).thenReturn(Optional.of(userTwo));
+        when(userRepository.save(userOne)).thenReturn(userTwo);
+        List<Task> result = taskUserService.addSharedTasks(1L, 2L);
+        assertEquals(tasks, result);
+        verify(userRepository, times(2)).findById(1L);
+        verify(userRepository, times(2)).findById(2L);
+    }
+
+    @Test
+    public void should_update_task_of_the_user_when_the_user_not_exist() {
+        Optional<User> userOptional = Optional.empty();
+        Task task = new Task(1L, "task3", false);
+        when(userRepository.findById(1L)).thenReturn(userOptional);
+        assertThrows(UserNotFoundException.class, () -> taskUserService.updateTaskForUser(1L, 1L, task));
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_update_task_of_the_user_when_the_users_exist_but_task_not_exist() {
+        Optional<Task> taskOptional = Optional.empty();
+        User userOne = new User(1L, "zhizhi");
+        Task task = new Task("task3", false);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
+        when(taskRepository.findById(1L)).thenReturn(taskOptional);
+        assertThrows(TaskNotFoundException.class, () -> taskUserService.updateTaskForUser(1L, 1L, task));
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    public void should_update_task_of_the_user_when_the_users_exist_and_task_exist_but_not_belong_to_the_user() {
+        Optional<Task> taskOptional = Optional.empty();
+        User userOne = new User(1L, "zhizhi");
+        Task task = new Task("task3", false);
+        Task returnedTask = new Task(1L, "zhizhi", false);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(returnedTask));
+        when(userRepository.findByTaskId(1L)).thenReturn(new ArrayList<>());
+        Task result = taskUserService.updateTaskForUser(1L, 1L, task);
+        assertNull(result);
+        verify(userRepository).findById(1L);
+        verify(taskRepository).findById(1L);
+        verify(userRepository).findByTaskId(1L);
+    }
+
+    @Test
+    public void should_update_task_of_the_user_when_the_users_exist_and_task_exist_and_belong_to_the_user() {
+        Optional<Task> taskOptional = Optional.empty();
+        User userOne = new User(1L, "zhizhi");
+        Task task = new Task("task3", false);
+        Task returnedTask = new Task(1L, "zhizhi", false);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(userOne));
+        when(taskRepository.findById(1L)).thenReturn(Optional.of(returnedTask));
+        when(userRepository.findByTaskId(1L)).thenReturn(List.of(userOne));
+        Task result = taskUserService.updateTaskForUser(1L, 1L, task);
+        Task expected = new Task(1L, "task3", false);
+        assertEquals(expected, result);
+        verify(userRepository).findById(1L);
+        verify(taskRepository).findById(1L);
         verify(userRepository).findByTaskId(1L);
     }
 }
